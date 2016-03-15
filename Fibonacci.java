bda7f15e0ed90aba3acabf8ba4fd6133ad2440e6
FredyEscalante <fesc_21@hotmail.com>
2016-03-15 17:48:21 -0600

    Added files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,43 @@
+
+import javax.swing.JOptionPane;
+public class Fibonacci 
+{
+    int s=0;
+    private void serie (int y)
+    
+    {
+        for(int i=0; i<=y; i++)
+        {
+            s=;
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main (String arg[])
+            
+     int x;
+    
+    {
+        x=Integer.parseInt(JOptionPane.showInputDialog(null,"inserta numero de veces"+obj.serie[x]));
+        Fibonacci obj=new Fibonacci();
+    }
+    
+    
+    
+    
+}
